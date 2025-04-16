011f2d3b4fd09e326ca1b3e2eb2d6f6d110dd304
Javier Claros <javiclaros@gmail.com>
2025-04-16 11:46:12 -0400

    login generando Token JWT

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
 import pool from "../db/connection";
 
+import bcrypt from "bcrypt";
+
+import jwt from "jsonwebtoken";
+
 export const addUsuario = (req: Request, res: Response) => {
-  const { nombre, password } = req.body;
+  const { body } = req;
 
   pool
     .query("INSERT INTO usuarios (nombre, password) VALUES (?, ?)", [
-      nombre,
-      password,
+      body.nombre,
+      bcrypt.hashSync(body.password, 10),
     ])
     .then((result) => {
       res.status(201).json({ msg: "Usuario creado" });
@@ -15,5 +19,44 @@ export const addUsuario = (req: Request, res: Response) => {
     .catch((err) => {
       console.error(err);
       res.status(500).json({ error: "Error creando usuario" });
+    });
+
+  // pool
+  //   .query("INSERT INTO usuarios (nombre, password) VALUES (?, ?)", body)
+  //   .then((result) => {
+  //     res.status(201).json({ msg: "Usuario creado" });
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     res.status(500).json({ error: "Error creando usuario" });
+  //   });
+};
+
+export const loginUser = (req: Request, res: Response) => {
+  const { body } = req;
+  console.log("body", body);
+
+  pool
+    .query("SELECT * FROM usuarios WHERE nombre = ?", [body.nombre])
+    .then((result) => {
+      if (result.length === 0) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+      const user = result[0];
+
+      if (!bcrypt.compareSync(body.password, user.password)) {
+        return res.status(401).json({ error: "Contraseña incorrecta" });
+      }
+
+      const token = jwt.sign(
+        { id: user.id, nombre: user.nombre },
+        process.env.SECRET_KEY!
+      );
+
+      res.status(200).json({ msg: "Login exitoso", user, token });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "Error en el login" });
     });
 };
